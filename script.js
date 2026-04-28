22648c934b35de2e168d70c9282c60ea1074b205
hazeeeeeel <jqiu_0926@163.com>
2026-04-28 02:25:32 -0400

    scroll issue

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ let div2 = document.getElementById('div2');
 let div3 = document.getElementById('div3');
 let img1 = document.getElementById('img1');
 let img2 = document.getElementById('img2');
+let container = document.querySelector('.container');
 
 div1.addEventListener('click', function() {
     img1.style.display = 'flex';
+    container.style.overflow = 'hidden';
 });
 
 div2.addEventListener('click', function() {
     img2.style.display = 'flex';
+    container.style.overflow = 'hidden';
 });
 div3.addEventListener('click', function() {
     window.open('https://jayeqiu.github.io/diaries-of-crocodiles-2.0/', '_blank');
@@ -20,4 +23,5 @@ function hideImgBox(id) {
     } else if (id === 2) {
         img2.style.display = 'none';
     }
+    container.style.overflow = 'scroll';
 }
